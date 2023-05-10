01f13f06b22da24962081f8ecc57f9a3281f9757
Leopls <leonardobento233@gmail.com>
2023-05-09 22:47:13 -0300

    update

--- site/cadastro-login/script-cadastro.js
+++ site/cadastro-login/script-cadastro.js
@@ -6,73 +6,75 @@ const cadastro_erro = document.querySelector('#cadastro_erro')
 
 const cadastro_btn = document.querySelector('#btn_cadastro')
 
-const numeros = [1,2,3,4,5,6,7,8,9,0]
-const sc = ['!','@','#','%','$','&','*','-','_']
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const sc = ['!', '@', '#', '%', '$', '&', '*', '-', '_']
 
 
-cadastro_btn.addEventListener('click', () =>  {
-   
+cadastro_btn.addEventListener('click', (e) => {
+
     const usuario = cadastro_usuario.value
     const email = cadastro_email.value
     const senha = cadastro_senha.value
     const senha_conf = cadastro_senhaconf.value
 
 
-    
-const verificarnumeros = numeros.some(item => senha.includes(item)) 
-const verificarsc = sc.some(e => senha.includes(e))
 
-const senha_validation = verificarnumeros && verificarsc && senha.toLowerCase() != senha && senha.length >=6 // VARIAVEL PARA VERIFICAR SENHA VÁLIDA
+    const verificarnumeros = numeros.some(item => senha.includes(item))
+    const verificarsc = sc.some(e => senha.includes(e))
 
-const email_validation =    // VARIAVEL PARA VERIFICAR EMAIL VÁLIDO
-email.indexOf('@') != -1 &&
-email.indexOf('.') != -1 &&
-email.indexOf(' ') == -1 &&
-email.substring(email.indexOf('@') + 1 , email.length).indexOf('@') == -1 &&
-email.endsWith('.') == false &&
-email.startsWith('.') == false && 
-email.startsWith('@') == false
+    const senha_validation = verificarnumeros && verificarsc && senha.toLowerCase() != senha && senha.length >= 6 // VARIAVEL PARA VERIFICAR SENHA VÁLIDA
+
+    const email_validation =    // VARIAVEL PARA VERIFICAR EMAIL VÁLIDO
+        email.indexOf('@') != -1 &&
+        email.indexOf('.') != -1 &&
+        email.indexOf(' ') == -1 &&
+        email.substring(email.indexOf('@') + 1, email.length).indexOf('@') == -1 &&
+        email.endsWith('.') == false &&
+        email.startsWith('.') == false &&
+        email.startsWith('@') == false
 
 
-    
-    if ( usuario == '' || email == '' || senha == '' || senha_conf == '' ){
-        cadastro_erro.innerHTML = 'Preencha todos os campos'
-        cadastro_erro.style.opacity = '1'
-    }else if(!email_validation){ 
+
+    if (usuario == '' || email == '' || senha == '' || senha_conf == '') {
+                                                                                                                                        
+    } else if (!email_validation) {
+        e.preventDefault()
         cadastro_erro.innerHTML = 'Email Inválido'
-        cadastro_erro.style.opacity = '1' ;
-        }else if (!senha_validation){
+        cadastro_erro.style.opacity = '1';
+    } else if (!senha_validation) {
+        e.preventDefault()
         cadastro_erro.innerHTML = 'Senha Inválida'
-        cadastro_erro.style.opacity = '1' 
-        }else if(senha != senha_conf){
-            cadastro_erro.innerHTML = 'Senhas diferentes'
-            cadastro_erro.style.opacity = '1' 
-        }else{
-            //TUDO VÁLIDO
-            alert('ok')
-        }
+        cadastro_erro.style.opacity = '1'
+    } else if (senha != senha_conf) {
+        e.preventDefault()
+        cadastro_erro.innerHTML = 'Senhas diferentes'
+        cadastro_erro.style.opacity = '1'
+    } else {
+        //TUDO VÁLIDO
+        alert('ok')
+    }
 
 
 })
 
 
 
-cadastro_email.addEventListener('focus', () =>{
+cadastro_email.addEventListener('focus', () => {
     cadastro_erro.innerHTML = ''
     cadastro_erro.style.opacity = '0'
 })
 
-cadastro_senha.addEventListener('focus', () =>{
+cadastro_senha.addEventListener('focus', () => {
     cadastro_erro.innerHTML = ''
     cadastro_erro.style.opacity = '0'
 })
-cadastro_usuario.addEventListener('focus', () =>{
+cadastro_usuario.addEventListener('focus', () => {
     cadastro_erro.innerHTML = ''
     cadastro_erro.style.opacity = '0'
 })
 
-cadastro_senhaconf.addEventListener('focus', () =>{
+cadastro_senhaconf.addEventListener('focus', () => {
     cadastro_erro.innerHTML = ''
     cadastro_erro.style.opacity = '0'
-    
+
 })
--- site/quiz/quiz.js
+++ site/quiz/quiz.js
@@ -1,3 +1,96 @@
+const perguntasRagnarok = [
+
 {
-    
+    pergunta: 'O que é o "Ragnarok? "',
+    alternativas: [
+        {resposta: 'É o jogo do god of war', correta: false},
+        {resposta: 'É o inverno da mitologia nórdica', correta: false},
+        {resposta: 'É  o fim do mundo na mitologia nórdica', correta: true},
+        {resposta: 'É quando odin tenta matar kratos em uma batalha por conta de suas atitudes passadas', correta: false},
+       
+    ]
+},
+
+{
+    pergunta: 'Quantos anos tem Atreus durante o jogo?',
+    alternativas: [
+        {resposta: '15', correta: false},
+        {resposta: '14', correta: true},
+        {resposta: '16', correta: false},
+        {resposta: '13', correta: false},
+       
+    ]
+},
+{
+    pergunta: 'Pergunta genérica',
+    alternativas: [
+        {resposta: '!true', correta: false},
+        {resposta: 'Carro de luxo 02 não é 01 ', correta: false},
+        {resposta: 'Capitalismo é bom', correta: false},
+        {resposta: 'Eu amo minha namorada (biapls)', correta: true},
+       
+    ]
+},
+{
+    pergunta: 'Pergunta genérica 2',
+    alternativas: [
+        {resposta: '!true', correta: false},
+        {resposta: 'Carro de luxo 02 não é 01 ', correta: false},
+        {resposta: 'Capitalismo é bom', correta: false},
+        {resposta: 'Eu amo minha namorada (biapls)', correta: true},
+       
+    ]
 }
+
+]
+
+const perguntaH2 = document.querySelector('#pergunta')
+const respostasDiv = document.querySelector('#respostas')
+const proximoBtn = document.querySelector('#btn-proximo')
+
+
+let questaoAtualPosicao = 0
+let pontuacao = 0
+
+
+function comecarQuiz(){
+    questaoAtual = 0
+    pontuacao = 0
+    proximoBtn.innerHTML = 'Próximo'
+    mostrarPergunta()
+}
+
+function mostrarPergunta(){
+
+    resetarQuestoesAnteriores()
+
+    let questaoAtual = perguntasRagnarok[questaoAtualPosicao]
+    let questaoNumero = questaoAtualPosicao + 1
+    perguntaH2.innerHTML = questaoNumero + ' - ' + questaoAtual.pergunta
+
+
+    questaoAtual.alternativas.forEach(alternativa => {
+        const button = document.createElement("button")
+        button.innerHTML = alternativa.resposta
+        button.classList.add("btn-resposta")
+        respostasDiv.appendChild(button)
+
+        if(alternativa.correta){
+            button.dataset.corret = alternativa.correta
+        }
+        button.addEventListener('click' , selecionarResposta)
+
+    } )
+
+}
+
+function resetarQuestoesAnteriores(){
+ proximoBtn.style.diplay = "none"
+ while(respostasDiv.firstChild){
+    respostasDiv.removeChild(respostasDiv.firstChild)
+ }
+}
+
+function selecionarResposta(){}
+
+comecarQuiz();
